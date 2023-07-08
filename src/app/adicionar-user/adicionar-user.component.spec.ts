e1e3333b4bcae344f35b579bf245467ad73e0d02
Guilherme Arantes <guii_arantes@hotmail.com>
2023-07-08 19:03:03 -0300

    first commit

---
+++ src/app/adicionar-user/adicionar-user.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdicionarUserComponent } from './adicionar-user.component';
+
+describe('AdicionarUserComponent', () => {
+  let component: AdicionarUserComponent;
+  let fixture: ComponentFixture<AdicionarUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdicionarUserComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdicionarUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/adicionar-user/adicionar-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-user',
+  templateUrl: './adicionar-user.component.html',
+  styleUrls: ['./adicionar-user.component.scss']
+})
+export class AdicionarUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
