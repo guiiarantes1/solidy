4c7a20f6a546e929acb38c0eca0255d484ea3206
Guilherme Arantes <guii_arantes@hotmail.com>
2023-07-11 13:42:19 -0300

    fix css scroll

--- src/app/gerenciar-user/gerenciar-user.component.ts
+++ src/app/gerenciar-user/gerenciar-user.component.ts
@@ -11,25 +11,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./gerenciar-user.component.scss'],
 })
 export class GerenciarUserComponent implements OnInit {
-  formCliente!: FormGroup;
+  formClienteEditado!: FormGroup;
   clientes: any = [];
   id!: any;
   clienteAtual:any = [];
-
+  clienteEditado:any = []
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.createForm(new Cliente());
-    console.log(this.clientes);
+
     let clientesLocalStorage = localStorage.getItem('clientes');
     console.log(clientesLocalStorage);
     this.clientes =
     clientesLocalStorage == null ? [] : JSON.parse(clientesLocalStorage);
-    console.log(this.clientes);
     localStorage.setItem('clientes', JSON.stringify(this.clientes));
   }
   createForm(cliente: Cliente) {
-    this.formCliente = this.formBuilder.group({
+    this.formClienteEditado = this.formBuilder.group({
       nome: [cliente.nome, Validators.required],
       telefone: [cliente.telefone, Validators.required],
       modelo: [cliente.modelo, Validators.required],
@@ -51,4 +50,17 @@ export class GerenciarUserComponent implements OnInit {
   }
 
 
+  editar(){
+    this.clienteEditado.push(this.formClienteEditado.value)
+    console.log(this.clientes[this.id])
+    console.log(this.clienteEditado[this.id])
+    if(this.clientes[this.id] != this.clienteEditado[this.id]){
+      this.clientes.splice(this.id, 1);
+       console.log(this.clientes)
+      this.clientes.push(this.clienteEditado[this.id])
+      console.log(this.clientes)
+    }else{
+      console.log('falhou')
+    }
+  }
 }
