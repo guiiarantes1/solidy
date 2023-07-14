3d761e1ad4111cc68dc712327844d63af6e7e5ca
Guilherme Arantes <guii_arantes@hotmail.com>
2023-07-14 17:34:06 -0300

    alteracoes calendario

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -8,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarioComponent implements OnInit {
   dataAtual: Date = new Date();
   diasCalendario: Date[] = [];
+  clientes: any = [];
+  id!: any;
+  diaSelecionado:any;
+  dataSelecionada!:any;
+  clientesDia:any = [];
+  diaPgt!:boolean;
+  diaAtual: Date = new Date();;
+  mesAtual!:any;
+  anoAtual!:any;
+  dataLocal:any;
+  datasReceber:any =[];
+
   constructor() { }
 
   ngOnInit(): void {
+
     this.construirCalendario();
+    let clientesLocalStorage = localStorage.getItem('clientes');
+    this.clientes =
+    clientesLocalStorage == null ? [] : JSON.parse(clientesLocalStorage);
+    localStorage.setItem('clientes', JSON.stringify(this.clientes));
+    this.clientes.sort(function(a:any,b:any) {
+      return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+  });
+  console.log(this.diasCalendario)
+  this.verificarDiasPgt()
+  this.verificarDiaAtual()
+
+
   }
   construirCalendario() {
     const ano = this.dataAtual.getFullYear();
@@ -46,5 +71,30 @@ export class CalendarioComponent implements OnInit {
       this.dataAtual.setMonth(this.dataAtual.getMonth() + offsetMes);
       this.dataAtual = new Date(this.dataAtual.getTime());
       this.construirCalendario();
+      console.log(this.dataAtual)
   }
+
+  getId(index: any) {
+    this.id =this.diasCalendario[index].getDate();
+    this.dataSelecionada = this.diasCalendario[index].getFullYear() + '-' + '0'+(this.diasCalendario[index].getMonth()+1)
+    + '-' + this.diasCalendario[index].getDate();
+    this.dataSelecionada.toString()
+    this.diaSelecionado = this.id;
+    this.clientesDia = this.clientes.filter((cliente:any) => cliente.dataPgt == this.dataSelecionada)
+    console.log(this.clientesDia)
+    console.log(this.dataSelecionada)
+  }
+
+  verificarDiasPgt(){
+    console.log(this.clientes[0].dataPgt )
+    console.log(this.datasReceber)
+    console.log(typeof this.datasReceber)
+    console.log(this.diasCalendario[20])
+  }
+
+  verificarDiaAtual(){
+    this.diaAtual.setHours(0);
+    this.diaAtual.setMinutes(0);
+    this.diaAtual.setSeconds(0);
+    }
 }
