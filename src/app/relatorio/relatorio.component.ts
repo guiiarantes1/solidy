94f22c46c89ca53f721445af802af8e2121e788b
Guilherme Arantes <guii_arantes@hotmail.com>
2023-07-16 18:44:32 -0300

    tentativa meses relatorio

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./relatorio.component.scss']
 })
 export class RelatorioComponent implements OnInit {
+  clientes: any = [];
+  meses:any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    let clientesLocalStorage = localStorage.getItem('clientes');
+    this.clientes =
+    clientesLocalStorage == null ? [] : JSON.parse(clientesLocalStorage);
+    localStorage.setItem('clientes', JSON.stringify(this.clientes));
+
+    this.clientes.sort(function(a:any,b:any) {
+      return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+  });
+
+  console.log(this.clientes);
+
+  this.criarMeses()
+  console.log(this.meses);
+
   }
 
+  criarMeses(){
+    this.meses.push({sigla:'jan',nome:'janeiro', qntAdesao: '3', valorAdesao: '2355', valorBoni:'', valorTotal: ''},
+    {sigla:'fev',nome:'fevereiro', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'mar',nome:'março', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'abr',nome:'abril', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'mai',nome:'maio', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'jun',nome:'junho', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'jul',nome:'julho', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'ago',nome:'agosto', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'set',nome:'setembro', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'out',nome:'outubro', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'nov',nome:'novembro', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''},
+    {sigla:'dez',nome:'dezembro', qntAdesao: '', valorAdesao: '', valorBoni:'', valorTotal: ''})};
 }
